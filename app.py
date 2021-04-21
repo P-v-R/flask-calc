e8f7fd3e68ef208dbaa3d22f6263b149b2ba12f9
Perryvon <Perry.j.vr@gmail.com>
2021-04-21 12:54:20 -0700

    completed calc

--- app.py
+++ app.py
@@ -1,1 +1,44 @@
-# Put your app in here.
+from flask import Flask, request
+from operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+@app.route('/add')
+def add_response():
+    """responds the sum of two numbers"""
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+
+    result = add(a,b)
+    return f"<h1>{result}</h1>"
+ 
+@app.route('/sub')
+def sub_response():
+    """responds the result of two numbers subtracted"""
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+
+    result = sub(a,b)
+    return f"<h1>{result}</h1>"
+ 
+@app.route('/mult')
+def mult_response():
+    """responds the result of two numbers multiplied"""
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+
+    result = mult(a,b)
+    return f"<h1>{result}</h1>"
+ 
+
+@app.route('/div')
+def div_response():
+    """responds the result of two numbers divided"""
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+
+    result = div(a,b)
+    return f"<h1>{result}</h1>"
+ 
+
+
